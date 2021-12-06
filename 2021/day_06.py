02d49591d621012592865aa437ef2d86e619e80a
Wouter van der Aart <wouter.van.der.aart@pwc.com>
2021-12-06 08:42:03 +0100

    Change: dict -> list

--- 2021/day_06.py
+++ 2021/day_06.py
@@ -4,25 +4,25 @@ FISH = [int(x) for x in INPUT.split(',')]
 
 
 def lantern_simulation(days: int) -> int:
-    fishy = {x: 0 for x in range(9)}
+    fishy = [0 for x in range(9)]
 
     for x in FISH:
         fishy[x] += 1
 
     for d in range(days):
-        fishy = {
-            0: fishy[1],
-            1: fishy[2],
-            2: fishy[3],
-            3: fishy[4],
-            4: fishy[5],
-            5: fishy[6],
-            6: fishy[7] + fishy[0],
-            7: fishy[8],
-            8: fishy[0]
-        }
+        fishy = [
+            fishy[1],
+            fishy[2],
+            fishy[3],
+            fishy[4],
+            fishy[5],
+            fishy[6],
+            fishy[7] + fishy[0],
+            fishy[8],
+            fishy[0]
+        ]
 
-    return sum(fishy.values())
+    return sum(fishy)
 
 
 def run_1():
