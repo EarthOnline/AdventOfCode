6b23fe3e3028638a8bb14f0c69ec837ab47e12c8
Wouter van der Aart <wouter.van.der.aart@pwc.com>
2021-12-07 06:48:23 +0100

    2021 - Day 7

--- 2021/day_07.py
+++ 2021/day_07.py
@@ -1,12 +1,17 @@
 INPUT = open("./input_files/input_07", "r").read().strip("\n")
+# INPUT = """16,1,2,0,4,2,7,1,2,14"""
+SUBMARINES = [int(x) for x in INPUT.split(',')]
+
+SUB_MIN = min(SUBMARINES)
+SUB_MAX = max(SUBMARINES)
 
 
 def run_1():
-    pass
+    return min(sum(map(lambda x: abs(x - p), SUBMARINES)) for p in range(SUB_MIN, SUB_MAX))
 
 
 def run_2():
-    pass
+    return min(sum(d * (d+1) // 2 for d in map(lambda x: abs(x - p), SUBMARINES)) for p in range(SUB_MIN, SUB_MAX))
 
 
 print(run_1())
