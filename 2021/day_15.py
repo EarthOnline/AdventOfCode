36a85ea4ac83dd76127b2613b4d3923e70eb063c
Wouter van der Aart <wouter.van.der.aart@pwc.com>
2021-12-15 17:00:15 +0100

    2021 - Day 15 - reworl

--- 2021/day_15.py
+++ 2021/day_15.py
@@ -1,5 +1,7 @@
 from itertools import product, chain
 
+from typing import List, Tuple
+
 INPUT = open("./input_files/input_15", "r").read().strip("\n")
 # INPUT = """1163751742
 # 1381373672
@@ -13,18 +15,17 @@ INPUT = open("./input_files/input_15", "r").read().strip("\n")
 # 2311944581"""
 
 
-def map_cave(text):
-    lines = text.split('\n')
-    for x, y in product(range(len(lines[0])), range(len(lines))):
-        yield (x, y), int(lines[y][x])
+def map_cave(cave: List[List[int]]) -> List[Tuple[Tuple[int, int], int]]:
+    for x, y in product(range(len(cave[0])), range(len(cave))):
+        yield (x, y), int(cave[y][x])
 
 
-CAVE = dict(map_cave(INPUT))
+CAVE = dict(map_cave([[int(x) for x in l] for l in INPUT.split('\n')]))
 
 
 def adjecent(x, y, max_x, max_y):
     if x > 0:
-        yield x -1, y
+        yield x - 1, y
     if x < max_x:
         yield x + 1, y
     if y > 0:
@@ -37,23 +38,19 @@ def lowest_route(cave):
     max_x = max(x for x, y in cave.keys())
     max_y = max(y for x, y in cave.keys())
 
-    been_there = set()
-    step = 0
-
     current_positions = {k: set() for k in range(10)}
     current_positions[0].add((0, 0))
+    been_there = set()
+    step = 0
     while (max_x, max_y) not in current_positions[0]:
         step += 1
-        new_positions = current_positions.pop(0)
+        new_positions = [p for p in current_positions.pop(0) if p not in been_there]
 
         for p in new_positions:
-            if p in been_there:
-                continue
             been_there.add(p)
 
-            posibilities = [c for c in adjecent(*p, max_x, max_y) if c not in been_there]
-            for pos in posibilities:
-                current_positions[cave[pos]].add(pos)
+            for posibility in [c for c in adjecent(*p, max_x, max_y) if c not in been_there]:
+                current_positions[cave[posibility]].add(posibility)
 
         current_positions = {k - 1: v for k, v in current_positions.items()}
         current_positions[9] = set()
@@ -65,17 +62,19 @@ def run_1():
     return lowest_route(CAVE)
 
 
-def run_2():
-    first_cave = [list(chain.from_iterable(map(lambda x: (int(x) + r - 1) % 9 + 1
-                                               , c) for r in range(5))) for c in INPUT.split('\n')]
+def expand_cave(cave, size):
+    max_x = max(x for x, y in cave.keys()) + 1
+    max_y = max(y for x, y in cave.keys()) + 1
+    first_cave = [list(chain.from_iterable([(cave[(x, y)] + r - 1) % 9 + 1 for x in range(max_x)]
+                                           for r in range(size))) for y in range(max_y)]
 
-    large_cave = []
-    for r in range(5):
+    for r in range(size):
         for line in first_cave:
-            large_cave.append(list(map(lambda x: (int(x) + r - 1) % 9 + 1, line)))
+            yield [(x + r - 1) % 9 + 1 for x in line]
 
-    cave = {(x, y): large_cave[y][x] for x, y in product(range(len(first_cave[0])), range(len(first_cave[0])))}
-    return lowest_route(cave)
+
+def run_2():
+    return lowest_route(dict(map_cave(list(expand_cave(CAVE, 5)))))
 
 
 print(run_1())
