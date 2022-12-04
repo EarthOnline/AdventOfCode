5cc5650a51ed508523344f0ba889e2eaaee6afc2
Wouter van der Aart <wouter.van.der.aart@pwc.com>
2022-12-04 06:43:22 +0100

    2022 - Day 04

--- 2022/day_04.py
+++ 2022/day_04.py
@@ -1,12 +1,14 @@
-INPUT = open("./input_files/input_xx", "r").read().strip("\n")
+INPUT = open("./input_files/input_04", "r").read().strip("\n")
+
+GROUPS = [tuple(tuple(int(z) for z in y.split('-')) for y in x.split(',')) for x in INPUT.split('\n')]
 
 
 def run_1():
-    return
+    return sum(1 for x, y in GROUPS if (x[0] >= y[0] and x[1] <= y[1]) or (y[0] >= x[0] and y[1] <= x[1]))
 
 
 def run_2():
-    return
+    return sum(1 for x, y in GROUPS if (x[0] <= y[1] and x[1] >= y[0]))
 
 
 print(run_1())
