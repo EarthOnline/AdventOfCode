80710f59629d6d17f3b890306c655744c309fe1c
Wouter van der Aart <wouter.van.der.aart@pwc.com>
2022-12-06 10:58:42 +0100

    2015 - Day 24

--- 2015/day_24.py
+++ 2015/day_24.py
@@ -1,22 +1,29 @@
-from itertools import combinations, chain
+from functools import reduce
+from itertools import combinations
 
 INPUT = open("./input_files/input_24", "r").read().strip("\n")
 
 PACKAGES = tuple(int(x) for x in INPUT.split('\n'))
 
 
+def min_sized_group_for_target(target):
+    groups = []
+    for r in range(1, len(PACKAGES)):
+        options = [set(x) for x in combinations(PACKAGES, r) if sum(x) == target]
+        if len(options) > 0:
+            groups = options
+            break
+    return groups
+
+
 def run_1():
     target = sum(PACKAGES) // 3
-
-    groups = list(chain.from_iterable(
-        [x for x in combinations(PACKAGES, r) if sum(x) == target] for r in range(1, len(PACKAGES))))
-    options = [(x, y, z) for x, y, z in combinations(groups, 3) if tuple(sorted(x + y + z)) == PACKAGES]
-
-    return options
+    return min(reduce((lambda x, y: x * y), g) for g in min_sized_group_for_target(target))
 
 
 def run_2():
-    return
+    target = sum(PACKAGES) // 4
+    return min(reduce((lambda x, y: x * y), g) for g in min_sized_group_for_target(target))
 
 
 print(run_1())
