7b8cce95cbfd5580e57e4fcb2f10b2639f9b75f7
Wouter van der Aart <wouter.van.der.aart@pwc.com>
2022-12-15 14:07:16 +0100

    2022 - Day 15

--- 2022/day_15.py
+++ 2022/day_15.py
@@ -1,12 +1,59 @@
+import string
+from functools import reduce
+
 INPUT = open("./input_files/input_15", "r").read().strip("\n")
 
 
+class Pair:
+    DIGITS = string.digits + '-'
+
+    def __init__(self, contruct):
+        _s, _b = contruct.split(': ')
+        self.sensor = tuple(int(''.join(y for y in x if y in self.DIGITS)) for x in _s.split(', '))
+        self.beacon = tuple(int(''.join(y for y in x if y in self.DIGITS)) for x in _b.split(', '))
+        self.distance = self.get_distance(self.sensor, self.beacon)
+
+    def blocked_at_y(self, y):
+        range_distance = self.distance - abs(self.sensor[1] - y)
+        if range_distance < 0:
+            return None
+        return self.sensor[0] - range_distance, self.sensor[0] + range_distance
+
+    @staticmethod
+    def get_distance(sensor, beacon):
+        s_x, s_y = sensor
+        b_x, b_y = beacon
+        return abs(s_x - b_x) + abs(s_y - b_y)
+
+
+PAIRS = [Pair(x) for x in INPUT.split('\n')]
+
+
+def join(ranges, range_x):
+    if range_x is None:
+        return ranges
+
+    untouched = set(r for r in ranges if r[0] > range_x[1] or r[1] < range_x[0])
+
+    if len(untouched) == len(ranges):
+        untouched.add(range_x)
+        return untouched
+
+    touched = set(r for r in ranges if r not in untouched)
+    touched.add(range_x)
+    untouched.add((min(s for s, e in touched), max(e for s, e in touched)))
+    return untouched
+
+
 def run_1():
-    return
+    return sum(y - x for x, y in reduce(join, map(lambda p: p.blocked_at_y(2000000), PAIRS), set()))
 
 
 def run_2():
-    return
+    for y in range(0, 4000001):
+        blocked = reduce(join, map(lambda p: p.blocked_at_y(y), PAIRS), set())
+        if len(blocked) > 1:
+            return (sorted(blocked, key=lambda i: i[0])[0][1] + 1) * 4000000 + y
 
 
 print(run_1())
---
+++ 2022/day_16.py
@@ -0,0 +1,13 @@
+INPUT = open("./input_files/input_16", "r").read().strip("\n")
+
+
+def run_1():
+    return INPUT
+
+
+def run_2():
+    return
+
+
+print(run_1())
+print(run_2())
